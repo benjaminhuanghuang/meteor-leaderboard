b0768f3f71c30c1384701891cf5f3408a715ff98
Benjamin Huang <bhuang@rms.com>
2018-04-11 18:32:01 -0700

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+## Reference 
+    - Your first meteor applicaiton
+
+## Setup   
+```
+    meteor create .     # create project under current folder
+    meteor add twbs:bootstrap     # add bootstrap
+    meteor add session            # add session package
+```
---
+++ client/templates/posts/post_list.js
@@ -0,0 +1,22 @@
+/*
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    }
+];
+*/
+// Template helper
+Template.postsList.helpers({
+    posts: function () {
+        return Posts.find();
+    }
+});
